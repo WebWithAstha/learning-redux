5c44aa976f17ffe7f3d2a13acd596fa6d48b67ad
WebWithAstha <lodhiastha85@gmail.com>
2024-06-05 18:41:09 +0530

    boilerplate

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      
+      <p className="read-the-docs">
+        Click to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
